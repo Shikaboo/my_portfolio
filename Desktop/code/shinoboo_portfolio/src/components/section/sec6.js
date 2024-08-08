8a78cb7ab9a6fcb26b49adb432d47f29aa2ca3bf
Shikaboo <sksdhkd0724@gmail.com>
2024-08-08 12:01:44 +0900

    08/07 저녁

---
+++ Desktop/code/shinoboo_portfolio/src/components/section/sec6.js
@@ -0,0 +1,70 @@
+import React, { useState, useRef, forwardRef, useEffect } from "react";
+import PublicBtn from "../common/project_btn";
+import { clones } from "../../utils/text";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import { Navigation, Pagination } from "swiper/modules";
+
+const Sec6 = forwardRef(({ sec6Ref }, ref) => {
+  const [swiperIndex, setSwiperIndex] = useState(0); // 페이지네이션용
+  const [swiper, setSwiper] = useState(null); // 슬라이드용
+
+  const handlePrev = () => {
+    swiper?.slidePrev();
+  };
+
+  const handleNext = () => {
+    swiper?.slideNext();
+  };
+
+  return (
+    <section className="sec sec6" ref={sec6Ref}>
+      <article>
+        <div className="sec6_tit">
+          <h2>WEB CLONE</h2>
+          <span></span>
+        </div>
+        <div className="clone_mockUp_wrap">
+          <Swiper
+            className="sec6_swiper"
+            onSwiper={(swiper) => setSwiper(swiper)}
+            onActiveIndexChange={(swiper) => setSwiperIndex(swiper.realIndex)}
+            modules={[Navigation, Pagination]}
+            slidesPerView={3}
+            spaceBetween={30}
+            pagination={{ clickable: true }}
+          >
+            {clones.map((clone) => (
+              <SwiperSlide key={clone.id}>
+                <figure className="slide_cont">
+                  <img className="project_img" src={clone.src} alt={clone.title} />
+                  <figcaption className="slide_txt">
+                    <h4>{clone.title}</h4>
+                    <p>
+                      {clone.description.split("\n").map((line, idx) => (
+                        <React.Fragment key={idx}>
+                          {line}
+                          <br />
+                        </React.Fragment>
+                      ))}
+                    </p>
+                    <PublicBtn page_link={clone.page_link} github_link={clone.github_link} />
+                  </figcaption>
+                </figure>
+              </SwiperSlide>
+            ))}
+          </Swiper>
+          <div className="navigation-buttons">
+            <button className="swiper-button-prev" onClick={handlePrev}>Prev</button>
+            <div className="pagination"/>
+            <button className="swiper-button-next" onClick={handleNext}>Next</button>
+          </div>
+        </div>
+      </article>
+    </section>
+  );
+});
+
+export default Sec6;
---
+++ Desktop/code/shinoboo_portfolio/src/utils/text.js
@@ -0,0 +1,74 @@
+import { page_imgBundle } from "./image";
+
+export const projects = [
+    {
+      id: 1,
+      title: "RENEWAL PROJECT",
+      src: `${page_imgBundle[1].src}` ,
+      explain: "대한민국의 치킨 브랜드인 지코바의 홈페이지를 리뉴얼 했습니다. ",
+      language: '#HTML #CSS #JAVASCRIPT',
+      Period: '2024.03.26 ~ 2024.04.22',
+      Library: '',
+      page_link: "/",
+      github_link: "/",
+    },
+    {
+      id: 2,
+      title: "FIRST TEAM PROJECT",
+      src: `${page_imgBundle[2].src}` ,
+      explain: "고궁에서 개최되는 야간 행사를 홍보하고 안내하는 사이트입니다. 처음으로 팀원들과 함께 작업한 자체 제작 사이트입니다.",
+      language: '#HTML #CSS #JAVASCRPT',
+      Period: '2024.05.16 ~ 2024.06.18',
+      Library: '#React #Vite #Swiper #Api #AOS #Axios',
+      page_link: "https://juramii.github.io/Team_Period/",
+      github_link: "https://github.com/JURAMII/Team_Period",
+    },
+    {
+      id: 3,
+      title: "SECOND TEAM PROJECT",
+      src:  `${page_imgBundle[3].src}`,
+      explain: "초등학생을 위한 수학 교육 사이트입니다. 3D scene을 이용한 입체적은 문제풀이와 로컬 스토리지를 이용한 자체 로그인 및 회원가입 기능을 가지고 있습니다.",
+      language: '#HTML #CSS #JAVASCRPT',
+      Period: '2024.07.08 ~ 2024.08.06',
+      Library: '#React #Vite #GSAP #Three.js #React-route-dom',
+      page_link: "https://thesandfox.github.io/ezdomath/",
+      github_link: "https://github.com/TheSandfox/ezdomath",
+    },
+  ];
+
+
+  export const clones = [
+    {
+      id: 1,
+      title: "Domino Pizza",
+      src:  `${page_imgBundle[0].src}`,
+      description: "도미노 피자 메인 페이지를 클로닝 했습니다. 미디어쿼리를 통한 반응형 작업과 JS를 통한 간단한 동적 기능을 추가했습니다.",
+      page_link: "https://shikaboo.github.io/cloning-domino/",
+      github_link: "https://github.com/Shikaboo/cloning-domino",
+    },
+    {
+      id: 2,
+      title: "Lotte Bizcar",
+      src:  `${page_imgBundle[4].src}`,
+      description: "롯데 비즈카 메인 페이지를 클로닝 했습니다. 미디어쿼리를 통한 반응형 작업 및 스와이퍼 cdn을 연결하여 슬라이드 기능과 JS를 통해 간단한 버튼 클릭 메뉴 기능을 구현했습니다.",
+      page_link: "https://lottebizcar.com",
+      github_link: "https://github.com/lottebizcar",
+    },
+    {
+      id: 3,
+      title: "국립공주박물관",
+      src:  `${page_imgBundle[4].src}`,
+      description: "국립 공주 박물관 메인 페이지를 클로닝 했습니다. position을 이용해 사이트가 꾸며져 있었기에 최대한 position을 사용하지 않는 선에서 메인 페이지를 따라해보려 노력했습니다, 앞선 클로닝 사이트와 마찬가지로 간단한 반응형 작업도 진행했습니다.",
+      page_link: "https://museum.go.kr",
+      github_link: "https://github.com/museum",
+    },
+    {
+      id: 4,
+      title: "SOCAR",
+      src:  `${page_imgBundle[4].src}`,
+      description: "SOCAR의 메인 페이지를 클로닝 해봤습니다. 리액트를 처음 배울 당시에 리액트 문법 및 컴포넌트 사용을 숙달하기 위해 클로닝을 진행했습니다.",
+      page_link: "https://socar.com",
+      github_link: "https://github.com/socar",
+    },
+  ];
+  
